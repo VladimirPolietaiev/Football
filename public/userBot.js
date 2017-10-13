adba02da86d783ff290545d3c6cf6998f6866218
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-10-13 20:51:19 +0300

    fixed  bot  41 - 2

--- public/userBot.js
+++ public/userBot.js
@@ -1,24 +1,59 @@
-//
-// Aggresive strategy "run-and-kick"- all players run to ball and kick it if possible to any direction
-//
 'use strict';
 
 function getPlayerMove(data) {
   console.log("data ", data)
-  // TODO : IMPLEMENT THE BETTER STRATEGY FOR YOUR BOT
+
   var currentPlayer = data.yourTeam.players[data.playerIndex];
   var ball = data.ball;
 
+  var sixthPartOfFieldWidth = data.settings.field.width / 6 ;
+  var playerZoneStartX = sixthPartOfFieldWidth * [0,1,2][data.playerIndex];
+  var playerZoneWidth = sixthPartOfFieldWidth * [3,4,4][data.playerIndex];
+
   var ballStop = getBallStats(ball, data.settings);
+  var direction = currentPlayer.direction;
+  var velocity = currentPlayer.velocity;
 
-  // var attackDirection = Math.atan2(ballStop.y - currentPlayer.y, ballStop.x - currentPlayer.x - ball.settings.radius);
-  var attackDirection = Math.atan2(ballStop.y - currentPlayer.y, ballStop.x - currentPlayer.x);
+  if ((ballStop.x > playerZoneStartX) && (ballStop.x < playerZoneStartX + playerZoneWidth)) {
+    // ball stops in the current player zone
 
-  return {
-    direction: attackDirection,
-    velocity: currentPlayer.velocity + data.settings.player.maxVelocityIncrement
+    if (ballStop.x > currentPlayer.x) {
+      // can go and kick it to the opponent side
+      direction = getDirectionTo(currentPlayer, ballStop);
+      velocity = data.settings.player.maxVelocity; // dont care about acceleration, game engine reduce it to max allowed value
+    } else {
+      // do not kick to the my goalpost, move to the position behind the ball
+      const ballRadius = ball.settings.radius;
+      var stopPoint = {
+        x: ballStop.x - ballRadius * 2,
+        y: ballStop.y + (ballStop.y > currentPlayer.y ?  - ballRadius : + ballRadius) * 2
+      }
+      direction = getDirectionTo(currentPlayer, stopPoint);
+      velocity = getDistance(currentPlayer, stopPoint);
+    }
+  } else {
+    var zonePoint = {
+      x: playerZoneStartX + 10,
+      y: ball.y + Math.random() * 40 - 20
+    };
+    direction = getDirectionTo(currentPlayer, zonePoint);
+    velocity = getDistance(currentPlayer, zonePoint) < 20 ? 0 : data.settings.player.maxVelocity;
+  }
+    return {
+    direction: direction,
+    velocity: velocity
   };
 }
+
+function getDirectionTo(startPoint, endPoint) {
+  return Math.atan2(endPoint.y - startPoint.y, ((endPoint.x)-10) - startPoint.x);
+}
+
+
+function getDistance(point1, point2) {
+  return Math.hypot(point1.x-point2.x, point1.y - point2.y);
+}
+
 
 function getBallStats(ball, gameSettings) {
   var stopTime = getStopTime(ball);
