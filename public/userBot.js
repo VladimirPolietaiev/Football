a26ea8ac2211ccb49031d2f1faa36bb847830557
Vladimir Polietaiev <vladimir.polietaiev@gmail.com>
2017-10-14 18:07:49 +0300

    fixed  defaultBot  27 - 0 and  protoUzerBot 70- 0

--- public/userBot.js
+++ public/userBot.js
@@ -4,7 +4,7 @@
 'use strict';
 
 function getPlayerMove(data) {
-  console.log("data ", data)
+  console.log("data ", data);
 
   var currentPlayer = data.yourTeam.players[data.playerIndex];
   var ball = data.ball;
@@ -21,27 +21,32 @@ function getPlayerMove(data) {
     // ball stops in the current player zone
 
     if (ballStop.x > currentPlayer.x) {
+
       // can go and kick it to the opponent side
       direction = getDirectionTo(currentPlayer, ballStop);
       velocity = data.settings.player.maxVelocity; // dont care about acceleration, game engine reduce it to max allowed value
     } else {
+
       // do not kick to the my goalpost, move to the position behind the ball
       const ballRadius = ball.settings.radius;
       var stopPoint = {
         x: ballStop.x - ballRadius * 2,
-        y: ballStop.y + (ballStop.y > currentPlayer.y ?  - ballRadius : + ballRadius) * 2
+        y: ballStop.y + (ballStop.y > currentPlayer.y ?  - ballRadius : + ballRadius) * 5
       }
       direction = getDirectionTo(currentPlayer, stopPoint);
       velocity = getDistance(currentPlayer, stopPoint);
     }
   } else {
+
     var zonePoint = {
       x: playerZoneStartX + 10,
       y: ball.y + Math.random() * 40 - 20
     };
     direction = getDirectionTo(currentPlayer, zonePoint);
-    velocity = getDistance(currentPlayer, zonePoint) < 20 ? 0 : data.settings.player.maxVelocity;
+    velocity = getDistance(currentPlayer, zonePoint) < 20 ? 1 : data.settings.player.maxVelocity;
+    // console.log(" velocity", + data.playerIndex + currentPlayer.velocity);
   }
+
     return {
     direction: direction,
     velocity: velocity
