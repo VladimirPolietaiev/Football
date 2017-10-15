1152ef06fc339356937fc9d81447bf6675fe0fcc
alex <alex.polietaiev@hotmail.com>
2017-10-15 07:15:23 +0000

    made userBot the main bot

--- public/defaultBot.js
+++ public/defaultBot.js
@@ -1,88 +1,73 @@
-//  Simple strategy : split the field into 3 parts (with 1:2:3 proportion) and each player will control its part.
-//
-//  Player 0 - goalkeeper
-//  Player 1 - defender
-//  Player 3 - playmaker
-//  -------------------------
-//  |   |      |            |
-//  |   |      |            |
-//  | 0 |  1   |     2      |
-//  |   |      |            |
-//  |   |      |            |
-//  -------------------------
-//
-//  Each player is responcible to kick out the ball from its zone.
 'use strict';
 
 //MAIN
 function getPlayerMove(data) {
-  var currentPlayer = data.yourTeam.players[data.playerIndex];
-  var ball = data.ball;
-  var sixthPartOfFieldWidth = data.settings.field.width / 6 ;
-  var playerZoneStartX = sixthPartOfFieldWidth * [0,1,2][data.playerIndex];
-  var playerZoneWidth = sixthPartOfFieldWidth * [3,4,4][data.playerIndex];
-  var ballStop = getBallStats(ball, data.settings);
-  var direction = currentPlayer.direction;
-  var velocity = currentPlayer.velocity;
 
-  if ((ballStop.x > playerZoneStartX) && (ballStop.x < playerZoneStartX + playerZoneWidth)) {
-    if (ballStop.x > currentPlayer.x) {
+    var currentPlayer = data.yourTeam.players[data.playerIndex];
+    var ball = data.ball;
 
-      // can go and kick it to the opponent side
-      direction = getDirectionTo(currentPlayer, ballStop);
-      velocity = data.settings.player.maxVelocity; // dont care about acceleration, game engine reduce it to max allowed value
+    var sixthPartOfFieldWidth = data.settings.field.width / 6;
+    var playerZoneStartX = sixthPartOfFieldWidth * [0, 1, 2][data.playerIndex];
+    var playerZoneWidth = sixthPartOfFieldWidth * [3, 4, 4][data.playerIndex];
+
+    var ballStop = getBallStats(ball, data.settings);
+    var direction = currentPlayer.direction;
+    var velocity = currentPlayer.velocity;
+
+    if ((ballStop.x > playerZoneStartX) && (ballStop.x < playerZoneStartX + playerZoneWidth)) {
+        if (ballStop.x > currentPlayer.x) {
+            direction = getDirectionTo(currentPlayer, ballStop);
+            velocity = data.settings.player.maxVelocity;
+        } else {
+            const ballRadius = ball.settings.radius;
+            var stopPoint = {
+                x: ballStop.x - ballRadius * 2,
+                y: ballStop.y + (ballStop.y > currentPlayer.y ? -ballRadius : +ballRadius) * 5
+            };
+            direction = getDirectionTo(currentPlayer, stopPoint);
+            velocity = getDistance(currentPlayer, stopPoint);
+        }
     } else {
-
-      // do not kick to the my goalpost, move to the position behind the ball
-      const ballRadius = ball.settings.radius;
-      var stopPoint = {
-        x: ballStop.x - ballRadius * 2,
-        y: ballStop.y + (ballStop.y > currentPlayer.y ?  - ballRadius : + ballRadius) * 5
-      };
-      direction = getDirectionTo(currentPlayer, stopPoint);
-      velocity = getDistance(currentPlayer, stopPoint);
+        var zonePoint = {
+            x: playerZoneStartX + 10,
+            y: ball.y + Math.random() * 40 - 20
+        };
+        direction = getDirectionTo(currentPlayer, zonePoint);
+        velocity = getDistance(currentPlayer, zonePoint) < 20 ? 1 : data.settings.player.maxVelocity;
     }
-  } else {
 
-    var zonePoint = {
-      x: playerZoneStartX + 10,
-      y: ball.y + Math.random() * 40 - 20
+    return {
+        direction: direction,
+        velocity: velocity
     };
-    direction = getDirectionTo(currentPlayer, zonePoint);
-    velocity = getDistance(currentPlayer, zonePoint) < 20 ? 1 : data.settings.player.maxVelocity;_
-  }
-
-  return {
-    direction: direction,
-    velocity: velocity
-  };
 }
 
 //SERVICES
 function getDirectionTo(startPoint, endPoint) {
-  return Math.atan2(endPoint.y - startPoint.y, endPoint.x - startPoint.x);
+    return Math.atan2(endPoint.y - startPoint.y, ((endPoint.x) - 15) - startPoint.x);
 }
 
 function getDistance(point1, point2) {
-  return Math.hypot(point1.x-point2.x, point1.y - point2.y);
+    return Math.hypot(point1.x - point2.x, point1.y - point2.y);
 }
 
 function getBallStats(ball, gameSettings) {
-  var stopTime = getStopTime(ball);
-  var stopDistance = ball.velocity * stopTime
-    - ball.settings.moveDeceleration * (stopTime + 1) * stopTime / 2;
+    var stopTime = getStopTime(ball);
+    var stopDistance = ball.velocity * stopTime
+        - ball.settings.moveDeceleration * (stopTime + 1) * stopTime / 2;
 
-  var x = ball.x + stopDistance * Math.cos(ball.direction);
-  var y = Math.abs(ball.y + stopDistance * Math.sin(ball.direction));
+    var x = ball.x + stopDistance * Math.cos(ball.direction);
+    var y = Math.abs(ball.y + stopDistance * Math.sin(ball.direction));
 
-  // check the reflection from field side
-  if (y > gameSettings.field.height) y = 2 * gameSettings.field.height - y;
+    // check the reflection from field side
+    if (y > gameSettings.field.height) y = 2 * gameSettings.field.height - y;
 
-  return { stopTime, stopDistance, x, y };
+    return {stopTime, stopDistance, x, y};
 }
 
 function getStopTime(ball) {
-  return ball.velocity / ball.settings.moveDeceleration;
+    return ball.velocity / ball.settings.moveDeceleration;
+
 }
 
 //RUN
--- public/userBot.js
+++ public/userBot.js
@@ -1,39 +1,55 @@
+//  Simple strategy : split the field into 3 parts (with 1:2:3 proportion) and each player will control its part.
+//
+//  Player 0 - goalkeeper
+//  Player 1 - defender
+//  Player 3 - playmaker
+//  -------------------------
+//  |   |      |            |
+//  |   |      |            |
+//  | 0 |  1   |     2      |
+//  |   |      |            |
+//  |   |      |            |
+//  -------------------------
+//
+//  Each player is responcible to kick out the ball from its zone.
 'use strict';
 
 //MAIN
 function getPlayerMove(data) {
-
     var currentPlayer = data.yourTeam.players[data.playerIndex];
     var ball = data.ball;
-
-    var sixthPartOfFieldWidth = data.settings.field.width / 6;
-    var playerZoneStartX = sixthPartOfFieldWidth * [0, 1, 2][data.playerIndex];
-    var playerZoneWidth = sixthPartOfFieldWidth * [3, 4, 4][data.playerIndex];
-
+    var sixthPartOfFieldWidth = data.settings.field.width / 6 ;
+    var playerZoneStartX = sixthPartOfFieldWidth * [0,1,2][data.playerIndex];
+    var playerZoneWidth = sixthPartOfFieldWidth * [3,4,4][data.playerIndex];
     var ballStop = getBallStats(ball, data.settings);
     var direction = currentPlayer.direction;
     var velocity = currentPlayer.velocity;
 
     if ((ballStop.x > playerZoneStartX) && (ballStop.x < playerZoneStartX + playerZoneWidth)) {
         if (ballStop.x > currentPlayer.x) {
+
+            // can go and kick it to the opponent side
             direction = getDirectionTo(currentPlayer, ballStop);
-            velocity = data.settings.player.maxVelocity;
+            velocity = data.settings.player.maxVelocity; // dont care about acceleration, game engine reduce it to max allowed value
         } else {
+
+            // do not kick to the my goalpost, move to the position behind the ball
             const ballRadius = ball.settings.radius;
             var stopPoint = {
                 x: ballStop.x - ballRadius * 2,
-                y: ballStop.y + (ballStop.y > currentPlayer.y ? -ballRadius : +ballRadius) * 5
+                y: ballStop.y + (ballStop.y > currentPlayer.y ?  - ballRadius : + ballRadius) * 5
             };
             direction = getDirectionTo(currentPlayer, stopPoint);
             velocity = getDistance(currentPlayer, stopPoint);
         }
     } else {
+
         var zonePoint = {
             x: playerZoneStartX + 10,
             y: ball.y + Math.random() * 40 - 20
         };
         direction = getDirectionTo(currentPlayer, zonePoint);
-        velocity = getDistance(currentPlayer, zonePoint) < 20 ? 1 : data.settings.player.maxVelocity;
+        velocity = getDistance(currentPlayer, zonePoint) < 20 ? 1 : data.settings.player.maxVelocity;_
     }
 
     return {
@@ -44,11 +60,11 @@ function getPlayerMove(data) {
 
 //SERVICES
 function getDirectionTo(startPoint, endPoint) {
-    return Math.atan2(endPoint.y - startPoint.y, ((endPoint.x) - 15) - startPoint.x);
+    return Math.atan2(endPoint.y - startPoint.y, endPoint.x - startPoint.x);
 }
 
 function getDistance(point1, point2) {
-    return Math.hypot(point1.x - point2.x, point1.y - point2.y);
+    return Math.hypot(point1.x-point2.x, point1.y - point2.y);
 }
 
 function getBallStats(ball, gameSettings) {
@@ -62,12 +78,11 @@ function getBallStats(ball, gameSettings) {
     // check the reflection from field side
     if (y > gameSettings.field.height) y = 2 * gameSettings.field.height - y;
 
-    return {stopTime, stopDistance, x, y};
+    return { stopTime, stopDistance, x, y };
 }
 
 function getStopTime(ball) {
     return ball.velocity / ball.settings.moveDeceleration;
-
 }
 
 //RUN
